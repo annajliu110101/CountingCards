f093a1b2b824396f8c5aeb36a8cc3788a3534062
annajliu110101 <annajliu110101@gmail.com>
2025-07-09 16:50:39 -0700

    Create strategy.py

---
+++ strategy.py
@@ -0,0 +1,28 @@
+# @title Strategy
+class Strategy(ABC):
+    """
+    Abstract base class for decision-making algorithms.
+    Students can create new strategies by subclassing this.
+    """
+    @abstractmethod
+    def decide(self, player, game):
+        """
+        Implement this method to define how a player should act.
+        It should call player.hit() or player.stand() as needed.
+        """
+        pass
+
+class GeneralStrategy(Strategy):
+  def decide(self, player, game):
+    if player.score < 17:
+      game._hit(player)
+    else:
+      game._stand(player)
+
+# @title Create a Strategy Here!  Implement method decide(player, game)
+
+class YourStrategy(Strategy):
+
+  def decide(self, player, game):
+    pass
+
