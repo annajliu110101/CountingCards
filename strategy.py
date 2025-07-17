d3cd94816b5a9e9cf03c02cade27363f4ecbab11
annajliu110101 <annajliu110101@gmail.com>
2025-07-17 11:10:59 -0700

    Update strategy.py
    Changed GeneralStrategy to DealerStrategy, so you don't have to add player each time

--- strategy.py
+++ strategy.py
@@ -15,12 +15,14 @@ class Strategy(ABC):
         """
         pass
 
-class GeneralStrategy(Strategy):
-  def decide(self, player:Player, game:Game) -> None:
-    if player.score < 17:
-      game._hit(player)
+class DealerStrategy(Strategy):
+  def __init__(player:Player):
+      self.player = player
+  def decide(self, game, verbose = False) -> None:
+    if self.player.score < 17:
+      game.deal(self._player, verbose = verbose)
     else:
-      game._stand(player)
+      game.skip(self._player, verbose = verbose)
 
 # @title Create a Strategy Here!  Implement method decide(player, game)
 
