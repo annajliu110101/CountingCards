0ae0b1915f492486f94516a3afc87e4e0960a629
annajliu110101 <annajliu110101@gmail.com>
2025-07-17 02:23:05 -0700

    Update game.py

--- game.py
+++ game.py
@@ -12,65 +12,58 @@ class Game(ABC):
   def __init__(self, players:List[BlackjackPlayer], dealer:Optional[Dealer] = None, comparer = DefaultCardComparer):
     self.pool = 0
     self.round = 0
-
     self._game = 0
 
     self._players = players
     self._dealer = dealer
+    
+    self._all_active_players = [dealer] + players
+    self._active_players = players
+    self._inactive_players = []
 
-    self._all_players = [dealer] + players
-
-    self._all_hands = pd.DataFrame(self.get_names(), index = None, columns = ["Names"])
-
-    self._scoreboard = pd.DataFrame(self.get_names(), index = None, columns = ["Names"])
-
+    self._cards_dealt = Hand()
+    
     self.deck = None
-    self.round:int
-    self.pool:int
     self._handle = display(DisplayHandle(), display_id=True)
 
 ###### Getters #######
+  def get_cards_dealt(self) -> pd.Series:
+    return pd.Series([self._cards_dealt.cards], name = "Cards Dealt")
   def get_scores(self) -> pd.Series:
-    return pd.Series([player.score for player in self._all_players], name = "Scores")
+    return pd.Series([player.score for player in self._all_active_players], name = "Scores")
 
   def get_chips(self) -> pd.Series:
-    return pd.Series([player.chips for player in self._all_players], name = "Chips")
+    return pd.Series([player.chips for player in self._all_active_players], name = "Chips")
 
   def get_names(self) -> pd.Series:
-    return pd.Series([player.name for player in self._all_players], name = "Names")
+    return pd.Series([player.name for player in self._all_active_players], name = "Names")
 
   def get_hands(self) -> pd.Series:
-    return pd.Series([p.get_hand() for p in self._all_players], name = "Hands")
+    return pd.Series([p.get_hand() for p in self._all_active_players], name = "Hands")
 
-  def get_actives(self) -> pd.Series:
-    return pd.Series([not p.is_done() for p in self._all_players], name = "Active")
+  def get_status(self) -> pd.Series:
+    return pd.Series([not p.is_done() for p in self._all_active_players], name = "Status")
 
-  def get_active_players(self) -> List[BlackjackPlayer]:
-    return [p for p in self._players if not p.is_done()]
+  def get_active_players(self, verbose = False) -> List[BlackjackPlayer]:
+    return [p for p in self._active_players if not p.is_done(verbose = verbose)]
+    
+  def get_pending(self):
+    return [p for p in self._active_players if p.is_waiting()]
 
+  def check_eligible_players(self, player):
+    if not player.is_eligible():
+      self._inactive_players.append(player)
+      self._all_active_players.remove(player)
+      self._active_players.remove(player)
+      return False
+    return True
+  
 ###### Output ########
-
-  def _update_scoreboard(self):
-    self._scoreboard["Scores"] = self.get_scores()
-    self._scoreboard["Hands"] = self.get_hands()
-    self._scoreboard["Chips"] = self.get_chips()
-    self._scoreboard["Active"] = self.get_actives()
-    self._handle.update(HTML(self._scoreboard.to_html(escape=False)))
-
-  def display_scoreboard(self, player = None):
+  def display(self, player = None):
     if not player:
-      self._update_scoreboard()
-      print()
+      self._handle.update(HTML(self.get_cards_dealt().to_html(escape=False)))
     else:
-      try:
-        print()
-        print()
         player.display()
-        print()
-        print()
-      except:
-        print("Player not found")
-
 ######## Game Functions ###########
 
   def _prompt_bet(self, player):
@@ -88,12 +81,14 @@ class Game(ABC):
           if bet < 1:
               print(f"Error: Minimum bet is $1.")
           elif bet > player.chips:
-              print(f"Error:  You only have {player.chips} chips.")
+              print(f"Error:  You only have {player.chips} chips.  Betting the rest.")
+              return player.bet(player.chips)              
           else:
               return player.bet(bet)
 
   def take_bets(self):
-    for player in self._players:
+    for player in self._active_players:
+      
       bet = self._prompt_bet(player)
       self.pool += bet
 
@@ -130,6 +125,8 @@ class Game(ABC):
   def after_round(self): ...
 ########## Player Functions ############
 
+
+
   def next(self, player):
     if player.has_strategy():
       print(f"{player.name}, Your Current Score is {player.score}.")
@@ -137,12 +134,12 @@ class Game(ABC):
     else:
       action = input(f"{player.name}, Your Current Score is {player.score}.\nPress 'y' to 'hit', or press any other key to 'stand': \n")
       if action.lower() == 'y':
-        card = self._hit(player)
+        card = self.deal(player)
 
         print(f"{player.name}: Current Score = {player.score}")
 
       else:
-        self._stand(player)
+        self.skip(player)
 
 class Blackjack(Game):
   def __init__(self, players:List[BlackjackPlayer]):
@@ -152,17 +149,19 @@ class Blackjack(Game):
 
     super().__init__(players, self.dealer, BlackjackCardComparer)
 
-  def _hit(self, player, verbose = True):
+  def deal(self, player, verbose = True):
     card = self.deck.draw()
     player.hit(card)
-
+    self._cards_dealt.add(card)
+    
     if verbose:
       print(f"{player.name} drew {str(card)}")
 
     return card
 
-  def _stand(self, player):
-    print(f"{player.name} with score {player.score} has decided to stand, skipping...")
+  def skip(self, player, verbose = True):
+    if verbose:
+      print(f"{player.name} [Score: {player.score}] has decided to stand, skipping...")
     player.stand()
 
 
@@ -173,14 +172,6 @@ class Blackjack(Game):
       return "lose"
     else:
       return "push"
-
-  def get_pending(self):
-    return [p for p in self._players if p.is_waiting()]
-
-  def display_cards_drawn(self):
-    hands = [p.get_hand() for p in self._all_participants]
-    self._all_hands[f"Round {self._game}"] = pd.Series(hands, name = "Hands")
-    display(HTML(self._all_hands.T.to_html(escape=False)))
 
   def _payout(self, player, outcome):
     bet = player.current_bet
@@ -219,13 +210,13 @@ class Blackjack(Game):
     print("-----------------")
     for _ in range(2):
       self.round += 1
-      for p in self._all_players:
+      for p in self._all_active_players:
           self._hit(p)
 
     print("-----------------\n")
     peek = None
 
-    for p in self._players:
+    for p in self._active_players:
       if not p.is_blackjack():
         continue
 
@@ -240,11 +231,10 @@ class Blackjack(Game):
           winnings = self._payout(p, "push")
 
   def loop_turns(self):
-    actives = self.get_active_players()
 
-    while actives:
+    while (actives:=self.get_active_players()):
       print("-----------------")
-      actives = self._play_round(actives)
+      self._play_round(actives)
       print("-----------------\n")
 
     print("------------------")
@@ -256,14 +246,14 @@ class Blackjack(Game):
 
   def _play_round(self, active_players:List[BlackjackPlayer]):
     self.round += 1
-    print(f"Round {self.round}")
+    
+    print(f"Round {self.round}:")
+    
     for p in active_players:
       self.next(p)
       clear_line()
       if p.is_21():
         p.stand()
-
-    return [p for p in active_players if not p.is_done(verbose = False)]
 
   def settle(self):
     pending_players = self.get_pending()
@@ -288,6 +278,6 @@ class Blackjack(Game):
     print("Game Over")
 
   def after_round(self):
-      self.display_cards_drawn()
-      for p in self._all_players:
+      self.display()
+      for p in self._all_active_players:
         p.reset()
