bcd598a49c969eab56750f1805a4d194b250035f
annajliu110101 <annajliu110101@gmail.com>
2025-07-17 18:23:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from participants import BlackjackPlayer
 from game import Blackjack
-
-if __name__ == '__main__':
+from visualization import WinRateVisualizer
+def run():
   players = [BlackjackPlayer("Anna"), BlackjackPlayer("Noe"), BlackjackPlayer("Daniel")]
 
   game = Blackjack(players)
 
   game.play_round()
+
+  
+def batched_run():
+  players = [BlackjackPlayer("Anna", strategy = HiLoStrategy), BlackjackPlayer("Noe", strategy = HiLoStrategy), BlackjackPlayer("Daniel", strategy = HiLoStrategy)]
+
+  game = Blackjack(players)
+
+  WinRateVisualizer(game).play(50)
+  
+if __name__ == '__main__':
+  batched_run()
+  
